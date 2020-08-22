2ca8a632dec8df32c47b22b4e86e453d771e5e5a
lj92458 <lj92458@gmail.com>
2020-08-22 08:11:32 +0800

    增加了交易状态查询功能 https://api.etherscan.io/api?module=transaction&action=getstatus&txhash=

--- config.js
+++ config.js
@@ -1,11 +1,13 @@
 const uniswapSDK = require('@uniswap/sdk')
 const ethers = require('ethers')
 
-//const chainId = uniswapSDK.ChainId.MAINNET
-const chainId = uniswapSDK.ChainId.ROPSTEN
+const chainId = uniswapSDK.ChainId.MAINNET
+//const chainId = uniswapSDK.ChainId.ROPSTEN
+const etherscanAPIKey = 'B3HWMMWXRKKDD6RQ69S2G5MCF6NTD35NZW'
+const infuraAPIKey = "34922f85dc48411c9f1312b6031a71d6"
 const provider = ethers.getDefaultProvider(chainId, {
-    etherscan: "B3HWMMWXRKKDD6RQ69S2G5MCF6NTD35NZW",
-    infura: "34922f85dc48411c9f1312b6031a71d6",
+    etherscan: etherscanAPIKey,
+    infura: infuraAPIKey,
     //alchemy:
 })
 const jsonWallet = `{"address":"b0d1435590b4f14a5f4414f93489945546162ffc","id":"5a63696f-1fc9-4591-9630-dcdac63dcf1e","version":3,"Crypto":{"cipher":"aes-128-ctr","cipherparams":{"iv":"0aeffda1cdfefa0a6f562aa74ede2818"},"ciphertext":"a05f204b244d12cdb393a8eba901ab01f1bf68da0bf7c0f29eeb6d6405c8f089","kdf":"scrypt","kdfparams":{"salt":"69ef4fc24f87877c2aa16ff0a22c18c31365dcc7aa350a6bf918751e0f0d3001","n":131072,"dklen":32,"p":1,"r":8},"mac":"88b47b8fb496072fd23352fd1a50f7b99d59c15e69141a686eb8135bdc83b18a"},"x-ethers":{"client":"ethers.js","gethFilename":"UTC--2020-08-10T07-30-06.0Z--b0d1435590b4f14a5f4414f93489945546162ffc","mnemonicCounter":"61238297b9e423d2975f57697dcfd834","mnemonicCiphertext":"2852e5451056a74e8a9c0d3a2bc77bd0","path":"m/44'/60'/0'/0/0","locale":"en","version":"0.1"}}`
@@ -55,6 +57,15 @@ function init() {
     }
 }
 
+async function mnemonicToJson(mnemonic, password) {
+    const myWallet = ethers.Wallet.fromMnemonic(mnemonic)
+
+    console.log(myWallet.address)
+    let jsonStr = await myWallet.encrypt(password)
+    console.log(jsonStr)
+    return [myWallet.address, jsonStr]
+}
+
 exports.init = init
 exports.chainId = chainId
 exports.provider = provider
@@ -71,3 +82,5 @@ switch (chainId) {
 exports.serverUri = serverUri
 
 exports.slippage = slippage
+exports.infuraAPIKey = infuraAPIKey
+exports.etherscanAPIKey = etherscanAPIKey
--- orderService.js
+++ orderService.js
@@ -4,6 +4,7 @@ const IUniswapV2Router02 = require('@uniswap/v2-periphery/build/IUniswapV2Router
 const JSBI = require('jsbi')
 const config = require('./config')
 const util = require("./util")
+const https = require('https');
 
 /* ethers.io使用手册：Contract对象
 调用某个智能合约，直接用address和abi构造Contractd对象。 这个对象的特性，请参考：
@@ -37,7 +38,7 @@ provider.waitForTransaction( hash [ , confirms = 1 [ , timeout ] ] )
 r/s/v参数：分别代表椭圆曲线签名的三个部分： transaction.r transaction.s transaction.v
  */
 
-const paramSet={}//去重，防止addOrder方法被莫名奇妙的重复调用
+const paramSet = {}//去重，防止addOrder方法被莫名奇妙的重复调用
 const defaultSlipage = util.doubleToPersent(config.slippage)
 
 config.init()
@@ -54,20 +55,20 @@ function logHead(transId) {
  * @param volume goods数量
  * @param maxWaitSeconds 在手续费不足的情况下，允许的超时时间。过了这个时间，就加速
  * @param gasPriceGwei gas价格Gwei
- * @param slippage 滑点 double
+ * @param slippage 滑点 double或string
  * @return AddOrderResult 对象
  */
 async function addOrder(coinPair, orderType, price, volume, maxWaitSeconds, gasPriceGwei, slippage) {
     let beginTime = new Date().getTime()
 
-    const key=coinPair+'_'+ orderType+'_'+ price+'_'+ volume+'_'+ maxWaitSeconds+'_'+ gasPriceGwei+'_'+ slippage
-        if(paramSet[key]){
-            console.info(logHead(beginTime)+" 重复调用："+key)
-            return null
-        }else{
-            console.info(logHead(beginTime)+" 首次调用："+key)
-            paramSet[key]=1
-        }
+    const key = coinPair + '_' + orderType + '_' + price + '_' + volume + '_' + maxWaitSeconds + '_' + gasPriceGwei + '_' + slippage
+    if (paramSet[key]) {
+        console.info(logHead(beginTime) + " 重复调用：" + key)
+        return null
+    } else {
+        console.info(logHead(beginTime) + " 首次调用：" + key)
+        paramSet[key] = 1
+    }
 
     //https://github.com/Uniswap/uniswap-v2-periphery/blob/master/contracts/UniswapV2Router02.sol
     const uniswapV2Router02 = new ethers.Contract("0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D", IUniswapV2Router02.abi, config.wallet)
@@ -98,8 +99,10 @@ async function addOrder(coinPair, orderType, price, volume, maxWaitSeconds, gasP
 
     //1.3 trade
     const trade = new uniswapSDK.Trade(route, inputAmount, uniswapSDK.TradeType.EXACT_INPUT)
+    console.info(`${logHead(beginTime)}应该输出${trade.outputAmount.toFixed(6)},不得低于${trade.minimumAmountOut(util.doubleToPersent(slippage)).toFixed(6)}`)
     //2.发送交易 .确定该调用uniswapV2Router02的哪个方法
     let allowedSlippage = slippage && slippage > 0 ? util.doubleToPersent(slippage) : defaultSlipage
+    //let allowedSlippage = slippage && slippage > 0 ? util.doubleToPersent(0.03) : defaultSlipage
     //{methodName: string  , args: (string | string[])[] , value: string}
     const swapParameters = uniswapSDK.Router.swapCallParameters(trade, {
         allowedSlippage: allowedSlippage,//滑点
@@ -107,7 +110,7 @@ async function addOrder(coinPair, orderType, price, volume, maxWaitSeconds, gasP
         recipient: config.wallet.address,
         feeOnTransfer: false //某些币，调用transfer/transferFrom时，会收费(对方收到金额小于我方发送的金额)
     })
-    console.info(`${logHead(beginTime)}fetchPairData，耗时：${Date.now() - beginTime},methodName:${swapParameters.methodName},value:${swapParameters.value}，slippage:${allowedSlippage.toFixed(3)}%`)
+    console.info(`${logHead(beginTime)}fetchPairData，耗时：${Date.now() - beginTime},methodName:${swapParameters.methodName},value:${swapParameters.value}，slippage:${allowedSlippage.toFixed(3)}%,swapParameters:${JSON.stringify(swapParameters)}`)
     /* overrides参数：
     gasLimit?: BigNumberish | Promise<BigNumberish>;
     gasPrice?: BigNumberish | Promise<BigNumberish>;
@@ -119,7 +122,14 @@ async function addOrder(coinPair, orderType, price, volume, maxWaitSeconds, gasP
 
 
     //let gasLimit = await uniswapV2Router02.estimateGas[swapParameters.methodName](...swapParameters.args, {value: swapParameters.value,})
-    let gasLimit = 140000
+    let gasLimit
+    if (swapParameters.methodName === 'swapExactETHForTokens') {
+        gasLimit = 140000
+    } else if (swapParameters.methodName === 'swapExactTokensForETH') {
+        gasLimit = 155000
+    } else {
+        gasLimit = 200000
+    }
 
     console.info(`${logHead(beginTime)}estimateGas耗时：${Date.now() - beginTime},sendEth(wei):${swapParameters.value},gasLimit:${gasLimit},gasPrice:${gasPriceGwei}Gwei`)
     let [packSucceed, transactionResponse, transactionReceipt] = await sendAndWait(
@@ -192,10 +202,88 @@ async function sendAndWait(uniswapV2Router02, swapParameters, gasPriceGwei, gasL
         packSucceed = false
     })
     if (packSucceed) {
-        console.info(`${logHead(transId)}waitForTransaction耗时：${Date.now() - beginTime},confirmations:${transactionReceipt.confirmations},gasUsed:${transactionReceipt.gasUsed}${transactionReceipt.gasUsed > 50000 ? ' ok' : ' 交易回滚'}`)
+        console.info(`${logHead(transId)}waitForTransaction耗时：${Date.now() - beginTime},confirmations:${transactionReceipt.confirmations},gasUsed:${transactionReceipt.gasUsed}`)
+
+        //查询交易状态：成功or失败
+        https.get(
+            'https://api.etherscan.io/api?module=transaction&action=getstatus&txhash=' +
+            transactionResponse.hash + '&apikey=' + config.etherscanAPIKey,
+
+            res => {
+                res.on('data', (d) => {
+                    //process.stdout.write(d+'\n');
+                    let result = JSON.parse(String(d))
+                    console.log(JSON.stringify(result))
+                    if (result.status === '1') {//如果接口调用成功
+                        //console.log(logHead(transId) + " 交易哈希:" + transactionResponse.hash + ",交易状态：" + result.result.isError === '1' ? '失败' : '成功' + ":" + result.result.errDescription)
+                    }
+                });
+            }
+        ).on('error', e => console.error(e))
     }
 
     return [packSucceed, transactionResponse, transactionReceipt]
 }
 
+//估算limit
+async function gasLimit_addOrder(coinPair, orderType, price, volume, maxWaitSeconds, gasPriceGwei, slippage) {
+    let beginTime = new Date().getTime()
+
+    const key = coinPair + '_' + orderType + '_' + price + '_' + volume + '_' + maxWaitSeconds + '_' + gasPriceGwei + '_' + slippage
+    if (paramSet[key]) {
+        console.info(logHead(beginTime) + " 重复调用：" + key)
+        return null
+    } else {
+        console.info(logHead(beginTime) + " 首次调用：" + key)
+        paramSet[key] = 1
+    }
+
+
+    const uniswapV2Router02 = new ethers.Contract("0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D", IUniswapV2Router02.abi, config.wallet)
+
+    //1.构建trade
+    //1.1构建route
+    const [goods, money] = coinPair.toLowerCase().split("-")
+    const [goodsObj, moneyObj] = [config.tokens[goods], config.tokens[money]]
+    if (!goodsObj || !moneyObj) {
+        throw Error("token 不存在：" + [goods, money])
+    }
+
+    const goodsToken = new uniswapSDK.Token(config.chainId, goodsObj.address, goodsObj.decimals, goodsObj.symbol)
+    const moneyToken = new uniswapSDK.Token(config.chainId, moneyObj.address, moneyObj.decimals, moneyObj.symbol)
+    const pair = await uniswapSDK.Fetcher.fetchPairData(goodsToken, moneyToken, config.provider)
+    const goodsCurrency = goods === 'eth' ? uniswapSDK.Currency.ETHER : goodsToken
+    const moneyCurrency = money === 'eth' ? uniswapSDK.Currency.ETHER : moneyToken
+
+    const inputCurrency = orderType === "buy" ? moneyCurrency : goodsCurrency
+    const outputCurrency = orderType === "buy" ? goodsCurrency : moneyCurrency
+    const route = new uniswapSDK.Route([pair], inputCurrency, outputCurrency)//output必须要自己填写。
+    //1.2 构建inputAmount
+    const amount = inputCurrency === moneyCurrency ? price * volume : volume
+    const inputAmount = inputCurrency === uniswapSDK.Currency.ETHER ?
+        uniswapSDK.CurrencyAmount.ether(util.movePointRight(amount, inputCurrency.decimals))
+        :
+        new uniswapSDK.TokenAmount(inputCurrency, util.movePointRight(amount, inputCurrency.decimals))
+
+    //1.3 trade
+    const trade = new uniswapSDK.Trade(route, inputAmount, uniswapSDK.TradeType.EXACT_INPUT)
+    //2.发送交易 .确定该调用uniswapV2Router02的哪个方法
+    let allowedSlippage = slippage && slippage > 0 ? util.doubleToPersent(slippage) : defaultSlipage
+    //{methodName: string  , args: (string | string[])[] , value: string}
+    const swapParameters = uniswapSDK.Router.swapCallParameters(trade, {
+        allowedSlippage: allowedSlippage,//滑点
+        ttl: maxWaitSeconds + 10 + 80,//时间限制:秒。因为参数构造好了，不是马上发出去，而是有延迟。所以要把延迟加上
+        recipient: config.wallet.address,
+        feeOnTransfer: false //某些币，调用transfer/transferFrom时，会收费(对方收到金额小于我方发送的金额)
+    })
+    console.info(`${logHead(beginTime)}fetchPairData，耗时：${Date.now() - beginTime},methodName:${swapParameters.methodName},value:${swapParameters.value}，slippage:${allowedSlippage.toFixed(3)}%`)
+
+
+    let gasLimit = await uniswapV2Router02.estimateGas[swapParameters.methodName](...swapParameters.args, {value: swapParameters.value,})
+    return gasLimit
+
+
+}
+
 exports.addOrder = addOrder
+exports.gasLimit_addOrder = gasLimit_addOrder
